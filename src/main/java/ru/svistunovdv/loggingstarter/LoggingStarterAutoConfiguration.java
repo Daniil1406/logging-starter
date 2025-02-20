e1b0721c3af845ad4af7a866ad5125e65ef6f422
Свистунов Даниил <daniil.svistunov@gazprombank.ru>
2025-02-20 19:33:45 +0300

    Create println method

---
+++ src/main/java/ru/svistunovdv/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,9 @@
+package ru.svistunovdv.loggingstarter;
+
+public class LoggingStarterAutoConfiguration {
+
+    public static void println(String string) {
+
+        System.out.println("Hello world!" + string);
+    }
+}
