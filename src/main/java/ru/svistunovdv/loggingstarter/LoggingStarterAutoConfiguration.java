fb8f6b521b701240643a8710a4607556342017d9
Свистунов Даниил <daniil.svistunov@gazprombank.ru>
2025-04-24 12:56:14 +0300

    Initial commit

--- src/main/java/ru/svistunovdv/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/svistunovdv/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,25 +1,30 @@
 package ru.svistunovdv.loggingstarter;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import ru.svistunovdv.loggingstarter.aspect.LogExecutionAspect;
 import ru.svistunovdv.loggingstarter.webfilter.WebLoggingFilter;
 import ru.svistunovdv.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
 
 @AutoConfiguration
+@ConditionalOnProperty(prefix = "logging", value = "enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingStarterAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(prefix = "logging", value = "log-exec-time", havingValue = "true")
     public LogExecutionAspect logExecutionAspect(){
         return new LogExecutionAspect();
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enabled", havingValue = "true", matchIfMissing = true)
     public WebLoggingFilter webLoggingFilter(){
         return new WebLoggingFilter();
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = {"enabled", "log-body"}, havingValue = "true")
     public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice(){
         return new WebLoggingRequestBodyAdvice();
     }
