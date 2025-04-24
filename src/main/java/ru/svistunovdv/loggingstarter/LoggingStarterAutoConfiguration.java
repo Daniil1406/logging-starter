8a32c6a615f6ac3b9dba94daf924ded261cb7d7c
Свистунов Даниил <daniil.svistunov@gazprombank.ru>
2025-04-24 12:45:52 +0300

    Initial commit

--- src/main/java/ru/svistunovdv/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/svistunovdv/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -3,6 +3,8 @@ package ru.svistunovdv.loggingstarter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import ru.svistunovdv.loggingstarter.aspect.LogExecutionAspect;
+import ru.svistunovdv.loggingstarter.webfilter.WebLoggingFilter;
+import ru.svistunovdv.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
 
 @AutoConfiguration
 public class LoggingStarterAutoConfiguration {
@@ -10,5 +12,17 @@ public class LoggingStarterAutoConfiguration {
     @Bean
     public LogExecutionAspect logExecutionAspect(){
         return new LogExecutionAspect();
-    };
+    }
+
+    @Bean
+    public WebLoggingFilter webLoggingFilter(){
+        return new WebLoggingFilter();
+    }
+
+    @Bean
+    public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice(){
+        return new WebLoggingRequestBodyAdvice();
+    }
+
+
 }
